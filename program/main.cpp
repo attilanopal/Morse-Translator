b59dba63ba74b9183a71522daaa431076d37c5f8
Rafi Alauddin <rafialauddin2014@gmail.com>
2021-06-10 01:22:21 +0700

    menu

--- program/main.cpp
+++ program/main.cpp
@@ -34,22 +34,43 @@ using namespace std;
 
 
 int main(){
+    int pilihan;
     Tree morseTree = nullptr;
     ptr New = nullptr;
     string morse,hasil;
     char lagi;
-    
-    createData(morseTree);
 
-    cout<<"\nMasukkan morse : ";cin>>morse;
-    hasil = decodeText(morse,morseTree,0);
-    
-    cout<<"\nApakah ingin menambah kata? (Y/N) : ";cin>>lagi;
-    while(lagi == 'y' || lagi == 'Y'){
-        hasil+=' ';
+    createData(morseTree);
+    cout<<"\n===================================================\n";
+    cout<<"SELAMAT DATANG DI PROGRAM PENERJEMAH SANDI MORSE\n";
+    cout<<"\nCreated by : Attila & Rafi\n";
+    cout<<"\n===================================================\n";
+    cout<<"\nTekan 1 Untuk Menerjemahkan Morse ke Huruf\n";
+    cout<<"Tekan 2 Untuk Menerjemahkan Huruf ke Morse\n";
+    cout<<"\nMasukkan Pilihan : "; cin >> pilihan;
+
+    switch (pilihan){
+    case 1:
+        cout<<"*Gunakan '/' sebagai pemisah Morse\n";
         cout<<"\nMasukkan morse : ";cin>>morse;
-        hasil+=decodeText(morse,morseTree,0);
-        cout<<"\nApakah ingin menambah kata? (Y/N)";cin>>lagi;
+        hasil = decodeText(morse,morseTree,0);
+    
+        cout<<"\nApakah ingin menambah kata? (Y/N) : ";cin>>lagi;
+        while(lagi == 'y' || lagi == 'Y'){
+            hasil+=' ';
+            cout<<"\nMasukkan morse : ";cin>>morse;
+            hasil+=decodeText(morse,morseTree,0);
+            cout<<"\nApakah ingin menambah kata? (Y/N)";cin>>lagi;
+        }
+        cout<<"\nHasil decrypt adalah : "<<hasil;
+        break;
+
+    case 2:
+        cout<<"*Gunakan '/' sebagai pemisah huruf\n";
+        //masukin codingan reverse nya til
+
+    default:
+        break;
     }
-    cout<<"\nHasil decrypt adalah : "<<hasil;
+
 }
